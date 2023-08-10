99c104fefc6f991caa554f479f9fa60899b2785a
Hwang Hyeeun <hwang.hyeeun@u.nus.edu>
2023-08-10 15:25:38 +0800

    lol

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-print("hello")
-print("wfuwofewenfo world")
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(columns=['Statutes_name','Statutes_id','Statutes_legalinterpretation','Statutes_legalproblem','Statutes_link'])
+
+df.loc[0] = ["Employment Act 1968","15.3","The notice to terminate the service of a person who is employed under……","Employment Termination","https://sso.agc.gov.sg/act/ema1968#pr10-"]
+
+df
